8cd5e65f9a7a1c070396ed902d087e34402543b8
Mc-Cristian <sangoluisacristian75@gmail.com>
2025-06-08 15:03:07 -0500

    pantalla login y registro

---
+++ lib/navigation/Drawer.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:app_peliculas/main.dart';
+import 'package:app_peliculas/screens/LoginScreen.dart';
+import 'package:app_peliculas/screens/RegistroScreen.dart';
+
+class MiDrawer extends StatelessWidget {
+  const MiDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(0xFF1A1A1A), // Fondo oscuro del Drawer
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFF101010), Color(0xFF2E2E2E)],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: const [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.white24,
+                    child: Icon(
+                      Icons.person,
+                      size: 50,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Bienvenido',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          _buildDrawerItem(
+            icon: Icons.login,
+            text: 'Iniciar Sesión',
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const LoginScreen()),
+              );
+            },
+          ),
+
+          _buildDrawerItem(
+            icon: Icons.person_add,
+            text: 'Registrarse',
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const RegistroScreen()),
+              );
+            },
+          ),
+
+          _buildDrawerItem(
+            icon: Icons.home,
+            text: 'Inicio',
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => const ProyectoF()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem({
+    required IconData icon,
+    required String text,
+    required VoidCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(icon, color: Colors.white),
+      title: Text(
+        text,
+        style: const TextStyle(color: Colors.white),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      hoverColor: Colors.grey.withOpacity(0.1),
+      onTap: onTap,
+    );
+  }
+}
